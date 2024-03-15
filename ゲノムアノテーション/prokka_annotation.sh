d29abf805c4ea2e565da42b3519340735392da49
fusho96 <85733410+fusho96@users.noreply.github.com>
2024-03-15 16:30:14 +0900

    update prokka on osx-arm64.sh

--- ゲノムアノテーション/prokka_annotation.sh
+++ ゲノムアノテーション/prokka_annotation.sh
@@ -3,11 +3,15 @@
 # Here I listed one possible way to solve this issue by creating an osx-64 environments.
 #Check more @ https://stackoverflow.com/questions/70205633/cannot-install-python-3-7-on-osx-arm64
 
-conda config --env --set subdir osx-64
-# If you are not osx-arm64 user, you don't need to run the command above!
-
-# INSTALL PROKKA IF NOT
+# INSTALL PROKKA IF NOT INSTALLED
 conda create -n prokka -c conda-forge -c bioconda -c defaults prokka
+
+# If you are not osx-arm64 user, you don't need to run those commands!
+# For osx-arm64 user 
+# conda config --env --set subdir osx-64
+# OR directly
+conda create CONDA_SUBDIR=osx-64 -n prokka -c conda-forge -c bioconda -c defaults prokka
+
 
 # ACTIVATE PROKKA ENV
 conda activate prokka
