60c8d44ec2cc1a897f2425e782cce5f991165b7f
fusho96 <85733410+fusho96@users.noreply.github.com>
2024-04-02 18:27:17 +0900

    Update for osx-arm64 again

--- ゲノムアノテーション/prokka_annotation.sh
+++ ゲノムアノテーション/prokka_annotation.sh
@@ -1,4 +1,4 @@
-
+=
 # Update on 2/Apr/2024: The update on 15/Mar/2024, might affect the total conda environment,
 # making installing other packages problems, thus,
 # here, I suggest to use docker to run the Prokka on Apple silicon processor.
@@ -12,7 +12,7 @@
 
 #For conda user
 
-# INSTALL PROKKA IF NOT INSTALLED
+# 1. INSTALL PROKKA IF NOT INSTALLED
 conda create -n prokka -c conda-forge -c bioconda -c defaults prokka
 
 # If you are not osx-arm64 user, you don't need to run those commands!
@@ -22,13 +22,13 @@ conda create -n prokka -c conda-forge -c bioconda -c defaults prokka
 # conda create CONDA_SUBDIR=osx-64 -n prokka -c conda-forge -c bioconda -c defaults prokka
 
 
-# ACTIVATE PROKKA ENV
+# 2. ACTIVATE PROKKA ENV
 conda activate prokka
 
-# MOVE TO WORKING DIR
+# 3. MOVE TO WORKING DIR
 cd prokka
 
-# ANNOTATE THE SEQUENCES
+# 4. ANNOTATE THE SEQUENCES
 prokka --kingdom Bacteria --outdir H1_3_1 --genus Achromobacter H1_3_1.fasta
     # --OUTDIR H1_3_1 : NAME OF THE OUTPUT DIR 
     # --genus Achromobacter : NAME OF THE GENUS
@@ -39,11 +39,18 @@ prokka --kingdom Bacteria --outdir LMG1863 --genus Achromobacter LMG_1863.fna
 # CHECK FOR MORE INFORMATION https://github.com/tseemann/prokka
 
 # For docker user
+
 # 1. download and install Docker Desktop for Macos (arm chips)
 # 2. pull the images
+docker pull staphb/prokka:latest
 # 3. create a volume for data exchanging with host
+docker volume create prokka
 # 4. copy the sequences into docker volume
+docker container create --name temp -v prokka:/data hello-world
+docker cp . temp:/data # make sure you have the seqs in the current dir
+docker rm temp
 # 5. run the prokka annotation
+docker run -v prokka:/data staphb/prokka:latest prokka seqs_name.fasta
 # 6. check the results inside Docker Desktop - Volume - Prokka
 
 
